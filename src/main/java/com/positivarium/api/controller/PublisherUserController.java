bb9b246eb932a67371042269776115ab68ddde13
Claire Sbaffe <claire.sbaffe@efrei.net>
2025-04-10 09:25:37 +0200

    FEAT user roles routes

---
+++ src/main/java/com/positivarium/api/controller/PublisherUserController.java
@@ -0,0 +1,15 @@
+package com.positivarium.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/publisher/users")
+public class PublisherUserController {
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok().body("This is ok, you can get in");
+    }
+
+}
