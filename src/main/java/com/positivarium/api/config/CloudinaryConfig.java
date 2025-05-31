db94ac7c1dd52931c0db4953f0e4af6ee0f2ea98
Claire Sbaffe <claire.sbaffe@efrei.net>
2025-05-31 13:28:57 +0200

    FEAT Cloudinary image uploader
    Updated pom.xml with Cloudinary dependencies.
    Added CloudinaryConfig.
    Added CloudinaryController and CloudinaryService. /api/cloudinary/upload route returns cloudinary image url.

---
+++ src/main/java/com/positivarium/api/config/CloudinaryConfig.java
@@ -0,0 +1,19 @@
+package com.positivarium.api.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Can be used anywhere in code thanks to @Configuration annotation
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${cloudinary.url}")
+    private String cloudinaryUrl;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(cloudinaryUrl);
+    }
+}
---
+++ src/main/java/com/positivarium/api/controller/CloudinaryImageController.java
@@ -0,0 +1,33 @@
+package com.positivarium.api.controller;
+
+import com.positivarium.api.service.CloudinaryImageUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/cloudinary")
+@RequiredArgsConstructor
+public class CloudinaryImageController {
+
+    private final CloudinaryImageUploadService cloudinaryImageUploadService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            System.out.println("Uploading image");
+            String imageUrl = cloudinaryImageUploadService.uploadImage(file);
+            System.out.println("Uploaded : " + imageUrl);
+            return ResponseEntity.ok(imageUrl);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Upload failed");
+        }
+    }
+}
